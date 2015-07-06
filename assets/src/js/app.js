54e7e1ceb8abfd7ea832f5532adc87ea1b06935d
Jimmy Multani <jimmy.multani@twistimage.com>
2015-07-06 16:53:22 -0400

    add more test elements to page, switch between slider and tabs, mimic elements in designs

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -71,21 +71,116 @@
 
 	// Example module for the homepage
 	MYAPP.accordion = (function() {
+		var windowWidth = window.innerWidth;
+
 		return {
 			init: function(media) {
 				console.log('accordion.init');
 
-				$('.accordion--' + media).accordion({
-					active: false,
-					heightStyle: 'content',
-					collapsible: true
-				});
+				if (typeof media === undefined) { media = 'desktop'; }
+
+				this.create(media);
+				this.addListeners();
+				this.updateAccordion();
+			},
+			create: function(media) {
+				// check if instance object exists to avoid console errors
+				if(!$('.accordion--' + media).accordion('instance')) {
+					$('.accordion--' + media).accordion({
+						active: false,
+						heightStyle: 'content',
+						collapsible: true
+					});
+				}
 			},
 			destroy: function(media) {
 				// check if instance object exists to avoid console errors
 				if($('.accordion--' + media).accordion('instance')) {
 					$('.accordion--' + media).accordion('destroy');
 				}
+			},
+			addListeners: function() {
+				$(window).on('resize', this.updateAccordion);
+			},
+			updateAccordion: MYAPP.helper.debounce(function() {
+				windowWidth = window.innerWidth;
+				console.log(windowWidth);
+
+				if(windowWidth >= 1024) {
+					// destroy first, init after
+					MYAPP.accordion.destroy('mobile');
+					MYAPP.accordion.create('desktop');
+				} else {
+					MYAPP.accordion.destroy('desktop');
+					MYAPP.accordion.create('mobile');
+				}
+			})
+		};
+	})();
+
+	MYAPP.tabs = (function() {
+		return {
+			init: function() {
+				console.log('tabs.init');
+
+				// if (typeof media === 'undefined') { media = 'mobile'; }
+
+				this.create();
+				this.addListeners();
+				this.updateTabs();
+			},
+			create: function() {
+				$('.tabs').tabs();
+			},
+			destroy: function() {
+				$('.tabs').tabs('destroy');
+			},
+			addListeners: function() {
+				$(window).on('resize', this.updateTabs);
+			},
+			updateTabs: MYAPP.helper.debounce(function() {
+				windowWidth = window.innerWidth;
+				console.log(windowWidth);
+
+				if(windowWidth >= 1024) {
+					if(!$('[data-break-mobile]').tabs('instance')) {
+						$('[data-break-mobile]').tabs();
+					}
+				} else {
+					// console.log($('[data-break-mobile]'));
+					if($('[data-break-mobile]').tabs('instance')) {
+						$('[data-break-mobile]').tabs('destroy');
+					}
+				}
+			})
+		};
+	})();
+
+	MYAPP.slider = (function() {
+		return {
+			init: function() {
+				console.log('slider.init');
+
+				this.create();
+			},
+			create: function() {
+				$('.slider').slick({
+					slidesToShow: 3,
+  					slidesToScroll: 3,
+					responsive: [
+						{
+							breakpoint: 1024,
+							settings: {
+								slidesToShow: 1,
+								slidesToScroll: 1,
+								infinite: true
+							}
+						}
+					]
+				});
+			},
+			destroy: function() {
+				$('.slider').unslick();
 			}
 		};
 	})();
@@ -95,19 +190,9 @@
 		var init = function() {
 			console.log('common.init');
 
-			$(window).on('resize', function() {
-				var windowWidth = window.innerWidth;
-
-				console.log(windowWidth);
-				if(windowWidth >= 1024) {
-					// destroy first, init after
-					MYAPP.accordion.destroy('mobile');
-					MYAPP.accordion.init('desktop');
-				} else {
-					MYAPP.accordion.destroy('desktop');
-					MYAPP.accordion.init('mobile');
-				}
-			}).resize();
+			MYAPP.accordion.init();
+			MYAPP.slider.init();
+			MYAPP.tabs.init();
 		};
 
 		return {
